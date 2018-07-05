4391dc590bddab72ba034e2018310566064f1f41
Biswash Adhikari <biswash.adhikari@bison.howard.edu>
2018-07-05 09:22:31 -0400

    second commit

--- app.js
+++ app.js
@@ -3,15 +3,30 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
-var params = {
-	q: 'This Is Us',
-	count: 2
-};
+var stream = T.stream('user');
 
-T.get('search/tweets', params, gotData);
+stream.on('tweet', tweetEvent);
 
-function gotData(err, data, response){
-	console.log(data)
-};
+function tweetEvent(eventMsg){
+	//console.log(eventMsg);
+	var fs= require('fs');
+	var json = JSON.stringify(eventMsg,null,2);
+	fs.writeFile("tweet.json", json);
+}
 
+function tweetIt(txt){
+	var tweet = {
+		status: txt
+	}
+
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, response) {
+		if(err){
+			console.log('something went wrong')
+		}else{
+			console.log('it worked')
+		}
+	}
+}
 
