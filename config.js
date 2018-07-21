fa4fbc07bd94a823ff64030044debca852d0f2a3
Andy-Rodr <and10rodr@gmail.com>
2018-07-21 03:13:16 -0700

    there we go. should add these into a .env file

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 module.exports = {
-	consumer_key: 'D4ZnuSgjnZEXYZtS2AXSwr9E0'
-  , consumer_secret: 'XuUclQaW4AkAgXH4jRELYmbCiVzr1Tncipf9tLse113mBZMjH7'
-  , access_token: '1015280687815933954-EcCnSBBuntn1sJfoJQAwfVfVr7nIyy'
-  , access_token_secret: 'vgYNDVJZIu2oF4SUhPonPYpWRPdxX0mQsCWw0VRjNWdrx'
-  , webhook_url: 'https://jurassic-world-mediatech-bot.herokuapp.com/webhooks/twitter'
-  , webhook_id: '1020401447291142144'
+	consumer_key: 'changeme'
+  , consumer_secret: 'changeme'
+  , access_token: 'changeme'
+  , access_token_secret: 'changeme'
+  , webhook_url: 'changeme'
+  , webhook_id: 'changeme'
 }
